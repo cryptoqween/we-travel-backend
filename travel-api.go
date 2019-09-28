5a3cf1146d4e0c917833f212a67b00d5df2825a4
cryptoqween <qtran@cryptocompare.com>
2019-09-28 15:13:28 +0100

    final api response

--- travel-api.go
+++ travel-api.go
@@ -17,12 +17,29 @@ type PathRequestBody struct {
 	ToLocation   [2]float64 `json:"toLocation"`
 }
 
-type MultiLineString struct {
+type PropertyOutput struct {
+	ID   string `json:"id"`
+	Type string `json:"type"`
+}
+
+type GeometryOutput struct {
 	Type        string       `json:"type"`
 	Coordinates [][2]float64 `json:"coordinates"`
 }
 
-func formatResponse(nodes []Node) [][2]float64 {
+type FeatureOutput struct {
+	Type       string         `json:"type"`
+	ID         string         `json:"id"`
+	Properties PropertyOutput `json:"properties"`
+	Geometry   GeometryOutput `json:"geometry"`
+}
+
+type GeoJsonOutput struct {
+	Type     string          `json:"type"`
+	Features []FeatureOutput `json:"features"`
+}
+
+func getCoordinates(nodes []Node) []Coordinate {
 	result := make([][2]float64, len(nodes))
 	for i := 0; i < len(nodes); i++ {
 		result[i] = nodes[i].value
@@ -40,12 +57,21 @@ func findpathHandler(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(reqBody, &newRequestBody)
 
 	nodes := calculatePath(newRequestBody.FromLocation, newRequestBody.ToLocation)
-	result := formatResponse(nodes)
+	coords := getCoordinates(nodes)
 
-	geojsonData := MultiLineString{Type: "MultiLineString", Coordinates: result}
+	geometry := GeometryOutput{Type: "LineString", Coordinates: coords}
+	featureOut := FeatureOutput{Type: "Feature", ID: "1234", Properties: PropertyOutput{}, Geometry: geometry}
+	features := make([]FeatureOutput, 1)
+
+	features[0] = featureOut
+
+	geojsonData := GeoJsonOutput{
+		Type:     "FeatureCollection",
+		Features: features,
+	}
+
 	geojsonDataInJson, _ := json.Marshal(&geojsonData)
 	w.Write(geojsonDataInJson)
-
 }
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
